2c94d4491e134c0f68be0bb355aeec7e24b901cd
Aubrey Wahl <aawahl@gmail.com>
2017-06-28 10:43:42 -0700

    one more test for testing travis

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, type: :model do
     User.create(email: 'aawahl@gmail.com', password: 'piss')
     expect(User.count).to eq 1
   end
+
+  it "has should not longer have users!" do
+    expect(User.count).to eq 0
+  end
 end
