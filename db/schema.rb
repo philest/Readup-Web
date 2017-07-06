6f68640e8c3c99817047fdbcbf638ff8bcf455fe
Aubrey Wahl <aawahl@gmail.com>
2017-07-06 13:34:41 -0700

    add school id field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706195812) do
+ActiveRecord::Schema.define(version: 20170706202939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170706195812) do
   create_table "classrooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "school_id"
   end
 
   create_table "schools", force: :cascade do |t|
@@ -60,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170706195812) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "school"
+    t.integer "school_id_default"
     t.index ["signature"], name: "index_teachers_on_signature", unique: true
   end
 
