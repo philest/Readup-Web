ae1c11bf8c417719e3d5df8dac15f20feec45767
Aubrey Wahl <aawahl@gmail.com>
2017-07-06 13:26:00 -0700

    fix school migration

--- db/migrate/20170706195812_create_schools.rb
+++ db/migrate/20170706195812_create_schools.rb
@@ -1,16 +1,14 @@
-class CreateSchoolTable < ActiveRecord::Migration[5.1]
+class CreateSchools < ActiveRecord::Migration[5.1]
   def change
-    create_table :school_tables do |t|
-
+    create_table :schools do |t|
       t.string :name
       t.string :signature
       t.string :zip_code
-      t.string :address
+      t.text :address
       t.string :phone
       t.text :email
       t.string :state
       t.string :city
-      t.string :last_name
       t.timestamps
     end
   end
