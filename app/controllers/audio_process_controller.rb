4e18fb51242ffdc52df1cf989ae410c2e8fa9c49
David Liu <davidliuliuliu@gmail.com>
2017-07-20 15:04:39 -0700

    direct upload from client side to S3 works. Need to fix incorrect response

--- app/controllers/audio_process_controller.rb
+++ app/controllers/audio_process_controller.rb
@@ -1,4 +1,5 @@
 class AudioProcessController < ApplicationController
+	before_action :set_s3_direct_post
 
 	def index
 		render 'audio'
@@ -27,4 +28,9 @@ class AudioProcessController < ApplicationController
 
 	end
 
+	private
+	def set_s3_direct_post
+    	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    	puts @s3_direct_post.fields
+  	end
 end
