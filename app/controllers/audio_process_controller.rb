0fe068b772a4872d85926a4359f3c2370921086b
Aubrey Wahl <aawahl@gmail.com>
2017-07-20 17:21:36 -0700

    clean up a few things

--- app/controllers/audio_process_controller.rb
+++ app/controllers/audio_process_controller.rb
@@ -1,42 +1,44 @@
 class AudioProcessController < ApplicationController
-	before_action :set_s3_direct_post
+  before_action :set_s3_direct_post
 
-	def index
-		render 'audio'
-	end
+  def index
+    render 'audio'
+  end
 
-	def save_file
-		puts (Time.now.to_f * 1000).to_i
-		audio = params[:audio]
-		save_path = Rails.root.join("public/")
-		save_path_complete = save_path + audio.original_filename
+  #
+  def save_file
+    audio = params[:audio]
+    save_path = Rails.root.join("public/")
+    save_path_complete = save_path + audio.original_filename
 
-		audio.rewind
+    audio.rewind
 
-	    # Open and write the file to file system.
-	  File.open(save_path_complete, 'wb') do |f|
-	    f.write audio.read
-	  end
-	  puts (Time.now.to_f * 1000).to_i
+    # Open and write the file to file system.
+    File.open(save_path_complete, 'wb') do |f|
+      f.write audio.read
+    end
 
-	  audio.rewind
+    audio.rewind
 
-	  AudioProcessWorker.perform_async(save_path, save_path_complete, audio.original_filename)
-	  
-	  puts (Time.now.to_f * 1000).to_i
-    	redirect_to action: 'index', status: 200
+    AudioProcessWorker.perform_async(save_path, save_path_complete, audio.original_filename)
 
-	end
+    redirect_to action: 'index', status: 200
+  end
 
+  #
+  def save_link
+    puts params[:awsRes]
+    redirect_to action: 'index', status: 201
+  end
 
-	def save_link
-		puts params[:awsRes]
-    	redirect_to action: 'index', status: 201
-	end
+  private
 
-	private
-	def set_s3_direct_post
-    	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-    	puts @s3_direct_post.fields
-  	end
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(
+      key: "uploads/#{SecureRandom.uuid}/${filename}",
+      success_action_status: '201',
+      acl: 'public-read',
+    )
+    puts @s3_direct_post.fields
+  end
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,10 +1,10 @@
 Aws.config = {
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  :region => 'us-west-1'
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_BUCKET_REGION'],
 }
 
 Aws.eager_autoload!
 
 # S3_BUCKET =  Aws::S3::Client.new.buckets[ENV['S3_BUCKET']]
-S3_BUCKET =  Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
