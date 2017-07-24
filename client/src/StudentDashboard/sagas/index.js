a0c3e208ecaf135d811f403e0d2903be9553f33d
Aubrey Wahl <aawahl@gmail.com>
2017-07-24 15:54:39 -0700

    put.resolve in key places

--- client/src/StudentDashboard/sagas/index.js
+++ client/src/StudentDashboard/sagas/index.js
@@ -93,21 +93,21 @@ function* getMicPermissions() {
 
   const hasPermissions = yield call(checkPermission)
   if (hasPermissions) {
-    yield put(setMicPermissions(MicPermissionsStatusOptions.granted))
+    yield put.resolve(setMicPermissions(MicPermissionsStatusOptions.granted))
     return true
   }
 
-  yield put(setMicPermissions(MicPermissionsStatusOptions.awaiting))
-  yield put(setCurrentOverlay('overlay-permissions'))
+  yield put.resolve(setMicPermissions(MicPermissionsStatusOptions.awaiting))
+  yield put.resolve(setCurrentOverlay('overlay-permissions'))
 
   // initialize
   const recorder =              yield select(getRecorder)
   const getPermissionSuccess =  yield call(getPermission, recorder)
 
-  yield put(setCurrentOverlay('no-overlay'))
+  yield put.resolve(setCurrentOverlay('no-overlay'))
 
   const micPermissions = getPermissionSuccess ? MicPermissionsStatusOptions.granted : MicPermissionsStatusOptions.blocked
-  yield put(setMicPermissions(micPermissions))
+  yield put.resolve(setMicPermissions(micPermissions))
   return micPermissions
 }
 
@@ -140,7 +140,7 @@ export default function* rootSaga() {
     return
   }
 
-  const exitTask = yield fork(onExit)
+  const exitClickedTask = yield fork(onExit)
 
   recorder = yield select(getRecorder)
 
@@ -148,34 +148,33 @@ export default function* rootSaga() {
   // soure: ???
   yield call(recorder.initialize)
 
-  yield put(setCurrentSound('/audio/book_intro.m4a'))
+  yield put.resolve(setCurrentSound('/audio/book_intro.m4a'))
 
 
   yield take(BOOK_INTRO_RECORDING_ENDED)
 
-  yield put(setReaderState(ReaderStateOptions.awaitingStart))
+  yield put.resolve(setReaderState(ReaderStateOptions.awaitingStart))
 
   yield take(START_RECORDING_CLICKED)
 
-  yield put(startCountdownToStart())
-  yield put(setPageNumber(1))
-  yield put(setReaderState(ReaderStateOptions.countdownToStart))
+  yield put.resolve(setPageNumber(1))
+  yield put.resolve(setReaderState(ReaderStateOptions.countdownToStart))
 
   yield take(COUNTDOWN_ENDED)
 
-  yield put(setReaderState(ReaderStateOptions.inProgress))
+  yield put.resolve(setReaderState(ReaderStateOptions.inProgress))
   recorder = yield select(getRecorder)
   yield call(recorder.startRecording)
-  yield put(setHasRecordedSomething(true))
+  yield put.resolve(setHasRecordedSomething(true))
 
-
-  yield cancel(exitTask)
+  // assign a new saga for exit clicking henceforth
+  yield cancel(exitClickedTask)
 
   yield takeLatest(EXIT_CLICKED, function* (payload) {
     recorder = yield select(getRecorder)
     yield call(recorder.pauseRecording)
-    yield put(setReaderState(ReaderStateOptions.paused))
-    yield put(setCurrentModal('modal-exit'))
+    yield put.resolve(setReaderState(ReaderStateOptions.paused))
+    yield put.resolve(setCurrentModal('modal-exit'))
   })
 
   yield takeLatest(TURN_IN_CLICKED, function* (payload) {
@@ -184,14 +183,14 @@ export default function* rootSaga() {
 
     // TODO submit the recording
 
-    yield put(setReaderState(ReaderStateOptions.submitted))
+    yield put.resolve(setReaderState(ReaderStateOptions.submitted))
 
     const isDemo = yield select(getIsDemo)
     if (isDemo) {
-      yield put(setCurrentOverlay('overlay-demo-submitted'))
+      yield put.resolve(setCurrentOverlay('overlay-demo-submitted'))
     }
     else {
-      yield put(setCurrentOverlay('overlay-submitted'))
+      yield put.resolve(setCurrentOverlay('overlay-submitted'))
       setTimeout(() => {
         window.location.href = "/" // TODO where to redirect?
       }, 5000)
@@ -203,49 +202,49 @@ export default function* rootSaga() {
   yield takeEvery(PAUSE_CLICKED, function* (payload) {
     recorder = yield select(getRecorder)
     yield call(recorder.pauseRecording)
-    yield put(setReaderState(ReaderStateOptions.paused))
-    yield put(setCurrentSound('/audio/paused.m4a'))
-    yield put(setCurrentModal('modal-paused'))
+    yield put.resolve(setReaderState(ReaderStateOptions.paused))
+    yield put.resolve(setCurrentSound('/audio/paused.m4a'))
+    yield put.resolve(setCurrentModal('modal-paused'))
     // directly show modal here
   })
 
   yield takeEvery(RESUME_CLICKED, function* (payload) {
     recorder = yield select(getRecorder)
     yield call(recorder.resumeRecording)
-    yield put(setReaderState(ReaderStateOptions.inProgress))
-    yield put(setCurrentModal('no-modal'))
+    yield put.resolve(setReaderState(ReaderStateOptions.inProgress))
+    yield put.resolve(setCurrentModal('no-modal'))
   })
 
   yield takeEvery(NEXT_PAGE_CLICKED, function* (payload) {
-    yield put({ type: PAGE_INCREMENT })
+    yield put.resolve({ type: PAGE_INCREMENT })
   })
 
   yield takeEvery(PREVIOUS_PAGE_CLICKED, function* (payload) {
-    yield put({ type: PAGE_DECREMENT })
+    yield put.resolve({ type: PAGE_DECREMENT })
   })
 
   yield takeEvery(RESTART_RECORDING_CLICKED, function* (action) {
     recorder = yield select(getRecorder)
     yield call(recorder.reset)
-    yield put(setCurrentModal('no-modal'))
-    yield put(setReaderState(ReaderStateOptions.awaitingStart))
-    yield put(setPageNumber(0))
+    yield put.resolve(setCurrentModal('no-modal'))
+    yield put.resolve(setReaderState(ReaderStateOptions.awaitingStart))
+    yield put.resolve(setPageNumber(0))
   })
 
 
 
   yield take(STOP_RECORDING_CLICKED)
 
-  yield put(setReaderState(ReaderStateOptions.done))
+  yield put.resolve(setReaderState(ReaderStateOptions.done))
   recorder = yield select(getRecorder)
   const blobURL = yield stopRecorderAndGetBlobURL(recorder)
 
-  yield put(setRecordingURL(blobURL))
-  yield put(setCurrentModal('modal-done'))
+  yield put.resolve(setRecordingURL(blobURL))
+  yield put.resolve(setCurrentModal('modal-done'))
   // yield call(recorder.forceDownloadRecording, ['_test_.wav'])
 
   yield takeEvery(HEAR_RECORDING_CLICKED, function* (action) {
-    yield put(setCurrentModal('modal-playback'))
+    yield put.resolve(setCurrentModal('modal-playback'))
   })
 
 
