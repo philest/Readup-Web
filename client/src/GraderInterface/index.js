ffb03d7e790c96cf26e9e978b1c1a7043c1d3d9e
philest <phil.esterman@yale.edu>
2017-09-21 22:57:21 -0400

    lang change to great

--- client/src/GraderInterface/index.js
+++ client/src/GraderInterface/index.js
@@ -22,7 +22,7 @@ import {
 
 
 const rubric = {
-  3: "Response shows exceptional understanding of Firefly Night. Includes all major events of plot in sequence, and shows insight into the girl's actions. Uses important details to enrich the retelling. Great job!",
+  3: "Response shows excellent understanding of Firefly Night. Includes all major events of plot in sequence, and shows insight into the girl's actions. Uses important details to enrich the retelling. Great job! Let me know if you have any questions.",
   2: "Response shows good understanding of Firefly Night. Includes major events of plot in sequence. Describes the the girl's actions and character, and uses some details to support the retelling. Nice job!",
   1: "Response shows partial understanding of Firefly Night. Includes one major event, but doesn't discuss others in-depth. Mentions main characters, but omits some details. Could use more focus on other parts of the story, but a good start.",
   0: "Response does not indicate understanding of Firefly Night. Includes only a part of a major event, and mentions main characters in a limited way. Facts stated are not in sequence, and important ones are left out."
