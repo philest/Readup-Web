03f7658f53e19fdcb0eff2240006fc72169a5c4b
philest <phil.esterman@yale.edu>
2017-09-25 14:19:55 -0400

    added selector

--- client/src/StudentDashboard/sagas/selectors.js
+++ client/src/StudentDashboard/sagas/selectors.js
@@ -1,4 +1,5 @@
 // selectors
 export const getRecorder = state => state.reader.recorder
 export const getIsDemo = state => state.reader.isDemo
+export const getNumQuestions = state => state.reader.book.numQuestions
 
