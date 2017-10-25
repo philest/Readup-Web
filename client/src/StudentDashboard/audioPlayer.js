120c72c525b3aa17546d2781c3c2e13b6c597ea6
philest <phil.esterman@yale.edu>
2017-10-25 14:46:25 -0600

    changes to recorder.js, beginning audioPlayer.js changes

--- client/src/StudentDashboard/audioPlayer.js
+++ client/src/StudentDashboard/audioPlayer.js
@@ -1,64 +1,89 @@
-export const DEV_DISABLE_VOICE_INSTRUCTIONS = false
+export const DEV_DISABLE_VOICE_INSTRUCTIONS = false;
 
-let audio = null
+let audio = null;
 
+const play = require("audio-play");
+const load = require("audio-loader");
+
+let stop = null;
 // TODO: Daniel fix this pls
 export function playSound(file, onEnd) {
   return new Promise((resolve, reject) => {
     if (DEV_DISABLE_VOICE_INSTRUCTIONS) {
-      resolve()
-      return
+      resolve();
+      return;
     }
 
-    if (!!audio) {
-      audio.pause()
+    resolve();
+    return;
+    try {
+      if (stop) {
+        stop();
+      }
+      load(file).then(buffer => {
+        stop = play(buffer);
+      });
+
+      resolve();
+      return;
+      stopAudio();
+
+      console.log("Playing Sound: " + file);
+
+      audio = new Audio(file);
+      audio.addEventListener("ended", function() {
+        audio = null;
+        console.log("audio ended");
+        resolve();
+      });
+      audio.addEventListener("error", function(error) {
+        console.log("audio error", error);
+        reject(error);
+      });
+      audio.play();
+    } catch (err) {
+      reject(err);
+      console.warn(err);
     }
-
-    console.log('Playing Sound: ' + file)
-
-    audio = new Audio(file);
-    audio.addEventListener('ended', function() {
-      audio = null
-      console.log("audio ended")
-      resolve()
-    });
-    audio.addEventListener('error', function(error) {
-      console.log("audio error")
-      reject(error)
-    })
-    audio.play();
-  })
+  });
 }
-
 
 export function playSoundAsync(file) {
-
-
-
+  return;
+  try {
     if (!!audio) {
-      audio.pause()
+      audio.pause();
 
-      // BEGIN toggle off audio when the same file is replayed 
-      let prevFileName = audio.src.substr(audio.src.lastIndexOf('/') + 1)
-      let currFileName = file.substr(file.lastIndexOf('/') + 1)
+      // BEGIN toggle off audio when the same file is replayed
+      let prevFileName = audio.src.substr(audio.src.lastIndexOf("/") + 1);
+      let currFileName = file.substr(file.lastIndexOf("/") + 1);
 
       if (prevFileName === currFileName) {
-        audio = null
-        return
+        audio = null;
+        return;
       }
       // END toggle off audio
-
     }
-    console.log('Playing Sound: ' + file)
+    console.log("Playing Sound: " + file);
     audio = new Audio(file);
     audio.play();
-
-}
-
-export function stopAudio() {
-  if (!!audio) {
-    audio.pause()
-    audio = null
+  } catch (err) {
+    console.warn(err);
   }
 }
 
+export function stopAudio() {
+  if (stop) {
+    stop();
+  }
+  return;
+  try {
+    if (!!audio) {
+      audio.pause();
+      audio.src = "";
+      audio = null;
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
--- client/src/StudentDashboard/recorder.js
+++ client/src/StudentDashboard/recorder.js
@@ -1,16 +1,15 @@
+import RecordRTC from "recordrtc";
+import DetectRTC from "detectrtc";
 
-import RecordRTC from 'recordrtc'
-import DetectRTC from 'detectrtc'
+import { sendEmail } from "../ReportsInterface/emailHelpers";
 
-import { sendEmail } from '../ReportsInterface/emailHelpers'
-
+global.RecordRTC = RecordRTC;
 
 class Recorder {
-
   constructor() {
-    this.rtcRecorder = null
-    this.blobURL = null
-    this.recording = false
+    this.rtcRecorder = null;
+    this.blobURL = null;
+    this.recording = false;
   }
 
   /* eslint-disable */
@@ -20,59 +19,71 @@ class Recorder {
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia ||
       navigator.msGetUserMedia
-    )
+    );
   }
   /*eslint-enable */
 
   static hasRecordingPermissions(callback) {
     DetectRTC.load(function() {
-      const hasPermissions = DetectRTC.isWebsiteHasMicrophonePermissions
-      callback(hasPermissions)
-    })
+      const hasPermissions = DetectRTC.isWebsiteHasMicrophonePermissions;
+      callback(hasPermissions);
+    });
   }
 
   captureUserMedia(callback) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(callback)
+      .catch(function(error) {
+        alert("Unable to access your microphone.");
+        console.error(error);
+        callback(null, error);
+      });
+    return;
+
     var params = { audio: true, video: false };
-    navigator.getUserMedia(params, (stream) => {
-      callback(stream)
-    }, (err) =>  {
-      callback(null, err)
-    })
-return
-    navigator.getUserMedia(params, callback, (error) => {
+    navigator.getUserMedia(
+      params,
+      stream => {
+        callback(stream);
+      },
+      err => {
+        debugger;
+        callback(null, err);
+      }
+    );
+    return;
+    navigator.getUserMedia(params, callback, error => {
       // alert(JSON.stringify(error));
-      console.log('USER MEDIA ERROR::   ' + JSON.stringify(error))
-      callback(null, error)
+      console.log("USER MEDIA ERROR::   " + JSON.stringify(error));
+      callback(null, error);
     });
-  };
+  }
 
-
-
-
-  initialize = (callback) => {
-
+  initialize = callback => {
+    return;
     if (!!this.rtcRecorder) {
-      console.log('Attempted to initialize an already initialized recorder but that\'s expected')
-      return
+      console.log(
+        "Attempted to initialize an already initialized recorder but that's expected"
+      );
+      return;
     }
 
-    console.log('initialize Recorder -- requestUserMedia')
+    console.log("initialize Recorder -- requestUserMedia");
     this.captureUserMedia((stream, error) => {
-
       if (error) {
-        console.log('!!errror capturing user media!!')
-        return callback && callback(error)
+        console.log("!!errror capturing user media!!");
+        return callback && callback(error);
       }
 
       // TODO: detect if system can play webms
 
       // <-- smaller filesize
       // this.rtcRecorder = RecordRTC(stream, { recorderType: RecordRTC.StereoAudioRecorder, bitsPerSecond: 30000, numberOfAudioChannels: 1, mimeType: 'audio/wav' });
-  
-      try {
 
+      try {
         // the MUAZ KHAN edits
-      /*  var hiddenAudio = document.createElement('audio');
+        /*  var hiddenAudio = document.createElement('audio');
         hiddenAudio.style.visibility = 'hidden';
         document.body.appendChild(hiddenAudio)
         hiddenAudio.srcObject = stream // this line is required to make sure stream tracks aren't stopped/released
@@ -80,114 +91,144 @@ return
         hiddenAudio.play()
 */
 
-        this.rtcRecorder = RecordRTC(stream,  { type: 'audio', mimeType: 'audio/webm', checkForInactiveTracks: true });
-        callback && callback(null)
-        return true
+        this.rtcRecorder = RecordRTC(stream, { type: "audio" });
+        callback && callback(null);
+        return true;
       } catch (err) {
-        sendEmail(err, "captureMedia (inner-most) startRecording failed", "philesterman@gmail.com")
-        console.log("captureMedia (inner-most) startRecording ERROR: ", err)
-        callback && callback(null)
-        return true
+        sendEmail(
+          err,
+          "captureMedia (inner-most) startRecording failed",
+          "philesterman@gmail.com"
+        );
+        console.log("captureMedia (inner-most) startRecording ERROR: ", err);
+        callback && callback(null);
+        return true;
       }
-
-
     });
   };
 
   reset = () => {
     if (this.recording) {
-      this.stopRecording()
+      this.stopRecording();
     }
-    this.rtcRecorder.reset()
-  }
+    this.rtcRecorder.reset();
+  };
 
   startRecording = () => {
-
+    const audio = (this.audio = document.createElement("audio"));
+    document.body.appendChild(audio);
     try {
       this.captureUserMedia((stream, error) => {
-
         if (error) {
-          console.log('error capturing user media (in startRecording)!!')
-          sendEmail(error, 'error capturing user media (in startRecording)', "philesterman@gmail.com")
-          return
+          console.log("error capturing user media (in startRecording)!!");
+          sendEmail(
+            error,
+            "error capturing user media (in startRecording)",
+            "philesterman@gmail.com"
+          );
+          return;
         }
+        this.stream = stream;
+        audio.muted = true;
+        audio.srcObject = stream;
+
+        audio.play();
 
         try {
-         this.initialize()
-    	   this.rtcRecorder.startRecording()
-         this.recording = true
-        }
-        catch (err) {
+          this.rtcRecorder =
+            this.rtcRecorder ||
+            RecordRTC(stream, {
+              recorderType: RecordRTC.StereoAudioRecorder,
+              mimeType: "audio/webm"
+            });
+          this.rtcRecorder.startRecording();
+          this.recording = true;
+        } catch (err) {
           try {
-            sendEmail(err, "inner startRecording failed", "philesterman@gmail.com")
-            console.log("inner startRecording ERROR: ", err)
-          }
-          catch (err2) {
-            console.log("inner startRecording ERROR and sendEmail failed: ", err2)
+            sendEmail(
+              err,
+              "inner startRecording failed",
+              "philesterman@gmail.com"
+            );
+            console.log("inner startRecording ERROR: ", err);
+          } catch (err2) {
+            console.log(
+              "inner startRecording ERROR and sendEmail failed: ",
+              err2
+            );
           }
         }
       });
     } catch (err) {
-        sendEmail(err, "startRecording failed", "philesterman@gmail.com")
-        console.log("startRecording ERROR: ", err)
+      sendEmail(err, "startRecording failed", "philesterman@gmail.com");
+      console.log("startRecording ERROR: ", err);
     }
+  };
 
-  }
-
-  stopRecording = (callback) => {
+  stopRecording = callback => {
     try {
       if (this.recording === true || this.rtcRecorder.state === "paused") {
+        /* let tracks = this.stream.getTracks();
+
+        tracks.forEach(function(track) {
+          track.stop();
+        })*/
+
+        this.stream.stop();
+        // this.audio.stop();
+        this.audio.srcObject = null;
         this.rtcRecorder.stopRecording(() => {
-          this.recording = false
-          this.blobURL = URL.createObjectURL(this.rtcRecorder.getBlob())
-          callback && callback(this.blobURL)
-        })
-      }
-      else {
-        sendEmail('startRecording', "startRecording never started", "philesterman@gmail.com")
-        console.log("startRecording ERROR: ", 'startRecording')
-        callback && callback('it broke')
+          this.recording = false;
+          this.blobURL = URL.createObjectURL(this.rtcRecorder.getBlob());
+          callback && callback(this.blobURL);
+        });
+      } else {
+        sendEmail(
+          "startRecording",
+          "startRecording never started",
+          "philesterman@gmail.com"
+        );
+        console.log("startRecording ERROR: ", "startRecording");
+        callback && callback("it broke");
       }
     } catch (err) {
-      sendEmail(err, "stopRecording failed", "philesterman@gmail.com")
-      console.log("stopRecording ERROR: ", err)
-      callback && callback('it broke')
+      sendEmail(err, "stopRecording failed", "philesterman@gmail.com");
+      console.log("stopRecording ERROR: ", err);
+      callback && callback("it broke");
     }
-  }
-
+  };
 
   pauseRecording = () => {
-    this.rtcRecorder.pauseRecording()
-    this.recording = false
-  }
+    this.rtcRecorder.pauseRecording();
+    this.recording = false;
+  };
 
   resumeRecording = () => {
-    this.rtcRecorder.resumeRecording()
-    this.recording = true
-  }
+    this.rtcRecorder.resumeRecording();
+    this.recording = true;
+  };
 
   isRecording = () => {
-    return this.recording
-  }
+    return this.recording;
+  };
 
   getBlob = () => {
-    return this.rtcRecorder.getBlob()
+    return this.rtcRecorder.getBlob();
   };
 
   getBlobURL = () => {
-    return this.blobURL
-  }
+    return this.blobURL;
+  };
 
   forceDownloadRecording(filename) {
-    console.log(this.getBlobURL())
-    var anchor = document.createElement('a')
-    anchor.href = this.blobURL
-    anchor.target = '_blank';
-    anchor.download = filename || 'output.wav';
+    console.log(this.getBlobURL());
+    var anchor = document.createElement("a");
+    anchor.href = this.blobURL;
+    anchor.target = "_blank";
+    anchor.download = filename || "output.wav";
     anchor.click();
   }
-
 }
 
-global.Recorder = Recorder
-export default Recorder
+global.Recorder = Recorder;
+export default Recorder;
