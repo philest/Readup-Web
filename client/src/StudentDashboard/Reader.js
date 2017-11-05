e29bc05fbf8d05936a3a0826f2ae292176ff93e8
philest <phil.esterman@yale.edu>
2017-11-05 13:55:32 -0700

    hide video if its a demo

--- client/src/StudentDashboard/Reader.js
+++ client/src/StudentDashboard/Reader.js
@@ -534,18 +534,19 @@ export default class Reader extends React.Component {
       <div className={styles.fullHeight}>
         {this.renderNavigationBar()}
 
-        {this.props.assessmentID && (
-          <div style={{ display: "none" }}>
-            <VideoChat
-              identity={"student"}
-              assessmentID={this.props.assessmentID}
-              room={`Assessment-${this.props.assessmentID}-Room`}
-              logs={false}
-              pictureInPicture={false}
-              hide
-            />
-          </div>
-        )}
+        {this.props.assessmentID &&
+          this.props.isDemo && (
+            <div style={{ display: "none" }}>
+              <VideoChat
+                identity={"student"}
+                assessmentID={this.props.assessmentID}
+                room={`Assessment-${this.props.assessmentID}-Room`}
+                logs={false}
+                pictureInPicture={false}
+                hide
+              />
+            </div>
+          )}
 
         <div
           className={
