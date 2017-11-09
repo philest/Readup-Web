1219dfde4a6aebdc2653fbd6cb5e074ec0820f97
philest <phil.esterman@yale.edu>
2017-11-09 13:12:20 -0700

    chrome version check for later

--- client/src/StudentDashboard/isSafari.js
+++ client/src/StudentDashboard/isSafari.js
@@ -9,4 +9,10 @@ const isSafari =
 			(typeof safari !== "undefined" && safari.pushNotification)
 	);
 
+export function getChromeVersion() {
+	var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+	return raw ? parseInt(raw[2], 10) : false;
+}
+
 export default isSafari;
