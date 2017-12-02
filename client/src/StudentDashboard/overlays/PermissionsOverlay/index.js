3fa9e8c2d75a7b8bd0f15cf5828ec8388f36b1d5
philest <phil.esterman@yale.edu>
2017-12-02 12:03:02 -0700

    an extra arrow

--- client/src/StudentDashboard/overlays/PermissionsOverlay/index.js
+++ client/src/StudentDashboard/overlays/PermissionsOverlay/index.js
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-import styles from './styles.css'
-import commonStyles from '../../modals/commonstyles.css'
+import styles from "./styles.css";
+import commonStyles from "../../modals/commonstyles.css";
 
-const THIS_OVERLAY_ID = 'overlay-permissions'
+const THIS_OVERLAY_ID = "overlay-permissions";
 
 export default class PermissionsModal extends React.Component {
   static propTypes = {
     onArrowClicked: PropTypes.func,
-    currentShowOverlay: PropTypes.string,
+    currentShowOverlay: PropTypes.string
   };
 
   /**
@@ -19,36 +19,46 @@ export default class PermissionsModal extends React.Component {
   constructor(props, _railsContext) {
     super(props);
     this.state = {
-      showReloadHelper: false,
+      showReloadHelper: false
     };
   }
 
   componentDidMount() {
-    setTimeout( () => { this.setState({showReloadHelper: true})}, 1800)
+    setTimeout(() => {
+      this.setState({ showReloadHelper: true });
+    }, 1800);
   }
 
   reloadPage = () => {
     window.location.reload();
-  }
+  };
 
   render() {
-
     if (this.props.currentShowOverlay !== THIS_OVERLAY_ID) {
-      return null
+      return null;
     }
 
     return (
       <div className={styles.permissionsContainer}>
-        <h1 style={{ color: 'white', fontSize: '2.5em' }}>
+        <h1 style={{ color: "white", fontSize: "2.5em" }}>
           Click "<i>Allow</i>" in the box above.
         </h1>
-        { this.state.showReloadHelper && 
-        <h4 onClick={this.reloadPage} className={styles.reloadHelper}>If no box shows, click here</h4>
-        }
-        <img src="/images/dashboard/green-up-arrow.png" className={styles.upArrow} onClick={this.props.onArrowClicked} />
+        {this.state.showReloadHelper && (
+          <h4 onClick={this.reloadPage} className={styles.reloadHelper}>
+            If no box shows, click here
+          </h4>
+        )}
+        <img
+          src="/images/dashboard/green-up-arrow.png"
+          className={styles.upArrow}
+          onClick={this.props.onArrowClicked}
+        />
+        <img
+          src="/images/dashboard/green-up-arrow.png"
+          className={styles.leftArrow}
+          onClick={this.props.onArrowClicked}
+        />
       </div>
-
-
     );
   }
 }
