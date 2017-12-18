860b530f9f9f0774561beb497426af92c6e1e692
philest <phil.esterman@yale.edu>
2017-12-18 12:36:06 -0700

    remove first answer from playback

--- client/src/StudentDashboard/modals/PlaybackModal/index.js
+++ client/src/StudentDashboard/modals/PlaybackModal/index.js
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-import styles from './styles.css'
-import commonStyles from '../commonstyles.css'
+import styles from "./styles.css";
+import commonStyles from "../commonstyles.css";
 
-import ModalHeader from '../subcomponents/ModalHeader'
-import RectangleButton from 'StudentDashboard/components/RectangleButton'
-import ButtonArray from '../subcomponents/ButtonArray'
+import ModalHeader from "../subcomponents/ModalHeader";
+import RectangleButton from "StudentDashboard/components/RectangleButton";
+import ButtonArray from "../subcomponents/ButtonArray";
 
-import BaseModal from '../BaseModal'
+import BaseModal from "../BaseModal";
 
-const THIS_MODAL_ID = 'modal-playback'
+const THIS_MODAL_ID = "modal-playback";
 
 export default class PlaybackModal extends React.Component {
   static propTypes = {
     audioSrc: PropTypes.string,
     compAudioSrc: PropTypes.string,
-    onStartOverClicked: PropTypes.func,  // TODO required?
+    onStartOverClicked: PropTypes.func, // TODO required?
     onTurnInClicked: PropTypes.func,
 
     currentShowModal: PropTypes.string,
     modalType: PropTypes.string,
-    showSpinner: PropTypes.bool,
+    showSpinner: PropTypes.bool
   };
 
   /**
@@ -30,53 +30,46 @@ export default class PlaybackModal extends React.Component {
    */
   constructor(props, _railsContext) {
     super(props);
-    this.state = {  };
+    this.state = {};
   }
 
   render() {
-
-    let firstIcons
+    let firstIcons;
 
     if (this.props.showSpinner) {
-      firstIcons = 'fa-spinner fa-pulse'
+      firstIcons = "fa-spinner fa-pulse";
     } else {
-      firstIcons = 'fa-check'
+      firstIcons = "fa-check";
     }
 
-
-
     return (
-      <BaseModal title='Your Recording' show={(this.props.currentShowModal === THIS_MODAL_ID)} modalType="info">
-
+      <BaseModal
+        title="Your Recording"
+        show={this.props.currentShowModal === THIS_MODAL_ID}
+        modalType="info"
+      >
         <div className={styles.playbackContainer}>
-          <h4 style={{ fontWeight: 'bold', marginTop: 20 }}>Hear reading</h4>
+          <h4 style={{ fontWeight: "bold", marginTop: 20 }}>Hear reading</h4>
           <audio controls autoPlay style={{ marginBottom: 20 }}>
             <source src={this.props.audioSrc} />
             <p>Playback not supported</p>
           </audio>
-          <h4 style={{ fontWeight: 'bold', marginTop: 20 }}>Hear first answer</h4>
-          <audio controls style={{ marginBottom: 20 }}>
-            <source src={this.props.compAudioSrc} />
-            <p>Playback not supported</p>
-          </audio>
- 
         </div>
 
         <div className={commonStyles.modalButtonArrayWrapper}>
           <ButtonArray
-            titles={['Turn it in', 'Mic broken']}
-            images={[firstIcons, 'fa-microphone-slash']}
-            actions={[this.props.onTurnInClicked, this.props.onStartOverClicked]}
+            titles={["Turn it in", "Mic broken"]}
+            images={[firstIcons, "fa-microphone-slash"]}
+            actions={[
+              this.props.onTurnInClicked,
+              this.props.onStartOverClicked
+            ]}
             fontAwesome={true}
             enlargeFirst={true}
             showSpinner={this.props.showSpinner}
           />
         </div>
-
-
       </BaseModal>
-
-
     );
   }
 }
